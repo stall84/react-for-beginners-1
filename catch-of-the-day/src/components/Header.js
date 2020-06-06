1496107cf86a00fd65719a5b8c7635ddf5886cf6
Michael Stallings <stall84@gmail.com>
2020-06-06 01:53:16 -0400

    Started using props to pass data in to the component between app.js and header.js

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends React.Component {
                         Day
                     </h1>
                     <h3 className="tagline">
-                    <span>FRESH DAILY</span>
+                    <span>{this.props.mainLine}</span>
                 </h3>
                 </header>
                 
