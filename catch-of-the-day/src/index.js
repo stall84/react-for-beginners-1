a2a7164c0c267fb8c81f6c595b5d72cc77f2c66a
Michael Stallings <stall84@gmail.com>
2020-06-06 01:19:05 -0400

    initial commit, structure of COD project built

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App';
+import StorePicker from './components/storePicker';
+import './css/style.css';
+
+
+
+
+
+
+
+render(<App />, document.querySelector('#main'));
+
