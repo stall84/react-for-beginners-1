f092ad90aeb7a950ab6406f38099a8ad1b81e3f7
Michael Stallings <stall84@gmail.com>
2020-06-08 12:30:04 -0400

    Went through different ways of getting input form data out of component. Numerous notes on ways to BIND THIS for custom made methods in component

--- catch-of-the-day/src/components/storePicker.js
+++ catch-of-the-day/src/components/storePicker.js
@@ -3,18 +3,43 @@ import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+    // Bind custom methods we make to component's 'this'
+    // by extending React's component's class constructor
+    constructor() {
+        // super to call on all of React.Component's base constructor setup
+        super();
+        // I hate what is about to follow: Bind each custom method to 'this'...
+        this.goToStore = this.goToStore.bind(this);
+        console.log('Gonna create a component')
+    }
+
+    // create empty ref to use in renders' input element
+    myInput = React.createRef();
+    // goToStore could also be defined as a property set equal to an arrow function to get around having to bind 'this' 
+    // like above.. would look like goToStore = (event) => {event.preventDefault(); ...}
     goToStore(event) {
+        // Stop form from refreshing/'submitting'
         event.preventDefault();
-        console.log('Going to Store')
+        // Get the text from user in input form (via ref)
+        console.log(this.myInput.current.value);
+
+        
     }
 
     render() {
+       
         return (
             <React.Fragment>
 
         <form className="store-selector" onSubmit={this.goToStore}>
             <h2> Please Select A Store </h2>
-            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+            <input 
+            type="text" 
+            ref={this.myInput}
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()} 
+            />
             <button type="submit">Visit Store -></button>
         </form>
             
