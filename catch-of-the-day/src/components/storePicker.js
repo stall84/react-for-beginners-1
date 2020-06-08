36a5d0e5f751f6c74da0114eebca5aa0aa61ddf3
Michael Stallings <stall84@gmail.com>
2020-06-07 20:32:09 -0400

    began coding out inline event handlers

--- catch-of-the-day/src/components/storePicker.js
+++ catch-of-the-day/src/components/storePicker.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault();
+        console.log('Going to Store')
+    }
+
     render() {
         return (
-        <form className="store-selector">
+            <React.Fragment>
+
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2> Please Select A Store </h2>
-            <input type="text" required placeholder="Store Name" />
+            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
             <button type="submit">Visit Store -></button>
         </form>
+            
+            </React.Fragment>
         )
     }
 }
